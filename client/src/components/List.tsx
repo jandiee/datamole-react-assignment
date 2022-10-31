106f50d8e566d3452873257c9808af566de90c5e
Jandiee <jandek.martin@gmail.com>
2022-10-31 10:16:52 +0100

    add flex grow to list component - stick footer to the bottom

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const StyledDiv = styled.div`
     display: flex;
     flex-direction: column;
+    flex: 1;
 `;
 
 export const List: React.FC<PropsWithChildren> = ({ children }) => <StyledDiv>{children}</StyledDiv>;
